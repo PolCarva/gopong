0f82dcfe813a00fb312e8ebe2443703a8d7004b2
Pablo Carvalho <107940742+PolCarva@users.noreply.github.com>
2025-09-23 15:47:44 -0300

    remove built with v0

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'GoPong',
+  description: 'Una web para el torneo de pingpong de GoPersonal',
 }
 
 export default function RootLayout({
